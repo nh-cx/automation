c9b59dc076ded3610b33d019f98fe90cad70b0c3
nh-cx <colin2002308@163.com>
2018-07-16 12:16:00 +0800

    updata

--- difflib_demo.py
+++ difflib_demo.py
@@ -3,14 +3,16 @@
 # 学习专用
 import difflib
 
-text1 = """text1:   #定义字符串1
+#定义字符串1
+text1 = """text1:   
 This module provides classes and functions for comparing sequences.
 including HTML and context and unified diffs.
 difflib document v7.4
 add string
 """
 
-text2 = """text2:   #定义字符串2
+#定义字符串2
+text2 = """text2:   
 This module provides classes and functions for Comparing sequences.
 including HTML and context and unified diffs.
 difflib document v7.5"""
@@ -18,10 +20,13 @@ difflib document v7.5"""
 text1_lines = text1.splitlines()
 text2_lines = text2.splitlines()
 
-d = difflib.Differ()
+# 常规输出
+# d = difflib.Differ()
+# diff = d.compare(text1_lines,text2_lines)
+# print('\n'.join(list(diff)))
 
-diff = d.compare(text1_lines,text2_lines)
 
-print('\n'.join(list(diff)))
+d = difflib.HtmlDiff()
+print(d.make_file(text1_lines, text2_lines))
 
 # if __name__ == "__main__":
